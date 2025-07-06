224e3d3a91d342da1c81fba14d58e573dba09595
dominikserwatka <dominikss20002@gmail.com>
2025-07-06 18:17:26 +0200

    new button to add to favorites

--- src/components/ProductListing.jsx
+++ src/components/ProductListing.jsx
@@ -2,18 +2,32 @@ import React from 'react';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { useFavorites } from '../context/FavoritesContext';
 
 function ProductListing({ product }) {
   const [showFullDescription, setShowFullDescription] = useState(false);
 
+  const [isHovered, setIsHovered] = useState(false);
+
   let description = product.description;
+
+  const { favorites, favoriteClick } = useFavorites();
+
+  const addedToFavorites = favorites.some((p) => p.id === product.id);
+
+  const buttonClass = addedToFavorites
+    ? 'absolute text-white mx-4 bg-red-500 px-2 py-2 rounded-md border-red transition'
+    : 'absolute text-black mx-4 px-2 py-2 rounded-md hover:bg-gray-900 hover:text-white transition'
+
 
   if (!showFullDescription) {
     description = description.substring(0, 60) + '...';
   }
 
   return (
-    <div className="bg-gray-50 rounded-xl shadow-lg overflow-hidden">
+    <div className="bg-gray-50 rounded-xl shadow-lg overflow-hidden"
+         onMouseEnter={() => setIsHovered(true)}
+         onMouseLeave={() => setIsHovered(false)}>
       <img src={product.picture} alt="Szafa przesuwna" className="w-full h-48 object-cover" />
       <div className="p-4 text-center">
         <h3 className="text-xl font-semibold mb-2">{product.name}</h3>
@@ -31,6 +45,15 @@ function ProductListing({ product }) {
         >
           Zobacz produkt
         </Link>
+        { isHovered && (
+                  <button
+          className={buttonClass}
+          onClick={() => favoriteClick(product)}
+        >
+          <i className="fa-regular fa-heart"></i>
+        </button>
+        )  }
+
       </div>
     </div>
   );
